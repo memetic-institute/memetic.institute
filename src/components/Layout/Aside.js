c2cc9c3b6354df12a102bf4208d3168919092bd0
NPC64927130 <NPC64927130@memetic.institute>
2020-03-26 06:02:49 -0600

    Update breakpoints

--- src/components/Layout/Aside.js
+++ src/components/Layout/Aside.js
@@ -2,7 +2,15 @@ import styled from 'styled-components';
 
 const Aside = styled.aside`
     flex: 1;
-    min-width: 400px;
+    min-width: 600px;
+
+    @media screen and (max-width: 1800px) {
+        min-width: 500px;
+    }
+
+    @media screen and (max-width: 1300px) {
+        min-width: 400px;
+    }
 
     @media screen and (max-width: 1000px) {
         min-width: 275px;
